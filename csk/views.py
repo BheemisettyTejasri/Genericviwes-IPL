7f199388217e186f965433209912ae4598e994b9
BheemisettyTejasri <tejasrib372@gmail.com>
2024-11-29 11:42:12 +0530

    first commit

---
+++ csk/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def captain(request):
+    return HttpResponse('<h1>king kohly is captain of csk</h1>')
+
+# Create your views here.
---
+++ rcb/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def Rcb(request):
+    return HttpResponse('</h1>rutu raj is captain of rcb')
+
+# Create your views here.
